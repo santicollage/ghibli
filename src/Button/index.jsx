11366c147859e6f64a4381e72befd13a24759cb2
Santiago Urrego <santiagourregolaya@gmail.com>
2024-08-24 23:31:56 -0500

    Icons import

--- src/Button/index.jsx
+++ src/Button/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Button.css';
+import arrowIcon1 from '../assets/icons/arrow-01.svg';
+import arrowIcon2 from '../assets/icons/arrow-02.svg';
+import camera from '../assets/icons/camera-01.svg';
+import list from '../assets/icons/list-01.svg';
 
 function Button({id, page, setPage, setMessage, setPhoto, width, height, icon, top, left}) {
 
